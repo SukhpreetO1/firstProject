5bcd2a0418e128dc887fcc89ae774dfbc5fe1ac9
SukhpreetO1 <ssingh77022@gmail.com>
2023-05-23 18:40:48 +0530

    spacing issue

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Roles extends Model
 {
-    public function u  ser()
+    public function user()
     {
         return $this->belongsTo(User::class, 'foreign_key');
     }
