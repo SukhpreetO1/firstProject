0554504d331b458e68ac231fad6bb2c6add67c12
SukhpreetO1 <ssingh77022@gmail.com>
2023-05-25 12:29:12 +0530

    changes in pagination

--- resources/views/crud/pagination.blade.php
+++ resources/views/crud/pagination.blade.php
@@ -1,9 +1,9 @@
 @if ($paginator->hasPages())
     <ul class="pager" style="list-style: none; display: flex; ">
         @if ($paginator->onFirstPage())
-            <li class="disabled"><span><- Previous</span></li>
+            <li class="disabled"><span> Previous</span></li>  
         @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">← Previous</a></li>
+            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev"> Previous</a></li>
         @endif
         @foreach ($elements as $element)
             @if (is_string($element))
@@ -20,9 +20,9 @@
             @endif
         @endforeach
         @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next →</a></li>
+            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next </a></li>
         @else
-            <li class="disabled"><span>Next →</span></li>
+            <li class="disabled"><span>Next </span></li>
         @endif
     </ul>
 @endif
